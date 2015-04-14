2653206e2bc60b932cc7c4915a8271a669e25e26
Christophe Espiau <christophe.espiau@korora.fr>
2015-04-14 13:50:58 +0200

    Bootstrap subtheme updated: page.vars.php added to customize content region width.

--- repository/themes/custom/mda/template.php
+++ repository/themes/custom/mda/template.php
@@ -4,3 +4,13 @@
  * @file
  * template.php
  */
+
+/**
+ * Implements hook_theme().
+ * 
+ */
+function mda_theme(&$existing, $type, $theme, $path) {
+	bootstrap_include($theme, 'theme/system/page.vars.php');
+	$hooks = array();
+	return $hooks;
+}
