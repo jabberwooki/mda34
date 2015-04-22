dcd2ddd252b738a531a6a602a78900eb643ac98f
Yvan Douënel <yvan@yvan-VPCEB3C5E.(none)>
2015-04-22 12:07:56 +0200

    CSS and tpl modified.

--- repository/themes/custom/mda/templates/page--front.tpl.php
+++ repository/themes/custom/mda/templates/page--front.tpl.php
@@ -104,7 +104,7 @@
     <div class="col-md-7">
       <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
         <div class="navbar-collapse collapse">
-          <nav role="navigation">
+          <nav role="navigation" id="main-top-menu">
             <?php if (!empty($primary_nav)): ?>
               <?php print render($primary_nav); ?>
             <?php endif; ?>
